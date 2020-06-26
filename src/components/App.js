fe5eabb9da64b47c90d861cd7a1df297cd12d7cb
Jagdish <jatin6972@gmail.com>
2020-06-27 00:49:52 +0530

    subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,43 @@ import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 
-function App() {
-  return (
-    <div className="App">
-      <h1></h1>
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favorite</div>
-        </div>
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    console.log("before state", store.getState());
+    store.subscribe(() => {
+      console.log("Updated");
+      // force render the state
+      this.forceUpdate();
+    });
+    // api call
+    // dispatch actions
+    this.props.store.dispatch({ type: "ADD_MOVIES", movies: data });
+    console.log("state", store.getState());
+  }
+  render() {
+    const movies = this.props.store.getState();
+    console.log("render");
+    return (
+      <div className="App">
+        <h1></h1>
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favorite</div>
+          </div>
 
-        <div className="list">
-          {/* usually forgot to put return  */}
-          {data.map((movie, index) => (
-            <MovieCard movie={movie} key={`movies=${index}`} />
-          ))}
+          <div className="list">
+            {/* usually forgot to put return  */}
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies=${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import movies from "./reducers/index.js";
 
 const store = createStore(movies);
 
-console.log(store);
-console.log("Before state", store.getState());
-// dispatch send action to reducer
-store.dispatch({
-  type: "ADD_MOVIES",
-  movies: [{ name: "Superman" }],
-});
+// console.log(store);
+// console.log("Before state", store.getState());
+// // dispatch send action to reducer
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "Superman" }],
+// });
 
-console.log("After state", store.getState());
+// console.log("After state", store.getState());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById("root")
 );
