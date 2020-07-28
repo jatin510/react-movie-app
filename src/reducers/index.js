3e2b66695a5d32936bd64bce7a899524f75f0df1
Jagdish <jatin6972@gmail.com>
2020-07-28 15:07:59 +0530

    added automatic reducers combining

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 import {
   ADD_MOVIES,
   ADD_TO_FAVOURITES,
@@ -55,9 +57,14 @@ const initialRootState = {
   search: initalSearchState,
 };
 
-export default function rootReducer(state = initialRootState, action) {
-  return {
-    movies: movies(state.movies, action),
-    search: search(state.search, action),
-  };
-}
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+
+export default combineReducers({
+  movies,
+  search,
+});
