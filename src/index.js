db9cc203b39344fa968668429a3c5a648b16e149
Jagdish <jatin6972@gmail.com>
2020-08-01 12:39:46 +0530

    logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers/index.js";
 
-const store = createStore(rootReducer);
+// function logger(obj, next, action)
+// currying
+// logger(obj)(next)(action)
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // middleware
+  console.log("ACTION_TYPE = ", action.type);
+  next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log(store.getState());
 // console.log("Before state", store.getState());
